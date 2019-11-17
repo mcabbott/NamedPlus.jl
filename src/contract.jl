26c2f31c163a201603ba5b2fcd8fe909305bb421
Michael Abbott <me@escbook>
2019-11-17 17:12:30 +0100

    contract generated func

--- src/contract.jl
+++ src/contract.jl
@@ -19,38 +19,80 @@ This is just a wrapper for `TensorOperations.contract!`.
 To multiply by a number `γ`, write it first. To conjugate all elements of one matrix,
 write `adjoint(A)`, which thanks to some piracy is no longer an error.
 """
-contract(A::NamedDimsArray{LA}, B::NamedDimsArray{LB}) where {LA,LB} =
-    contract(A, B, filter(n -> Base.sym_in(n, LB), LA))
+contract(A::NamedDimsArray, B::NamedDimsArray, s::Symbol) = contract(A, B, Val((s,)))
 
-contract(A::NamedDimsArray, B::NamedDimsArray, s::Symbol) =
-    contract(A, B, (s,))
+@generated function contract(A::NamedDimsArray{LA}, B::NamedDimsArray{LB}) where {LA,LB}
+    s = filter(n -> Base.sym_in(n, LB), LA)
+    :(contract(A, B, Val($s)))
+end
 
-function contract(A::NamedDimsArray{LA,TA,NA}, B::NamedDimsArray{LB,TB,NB}, s::Tuple{Vararg{Symbol}}) where {LA,LB,TA,TB,NA,NB}
-    oindA = filter(d -> !Base.sym_in(LA[d], s), ntuple(identity, NA))
-    cindA = filter(d -> Base.sym_in(LA[d], s), ntuple(identity, NA))
 
-    oindB = filter(d -> !Base.sym_in(LB[d], s), ntuple(identity, NB))
-    cindB = filter(d -> Base.sym_in(LB[d], s), ntuple(identity, NB))
+@generated function contract(γ::Number, A::NamedDimsArray{LA,TA,NA}, B::NamedDimsArray{LB,TB,NB}, ::Val{s}) where {LA,LB,TA,TB,NA,NB,s}
+    cindA = NamedDims.dim(LA, s)
+    oindA = Tuple(setdiff(1:NA, cindA))
 
-    indCinoAB = (ntuple(identity, length(oindA))..., ntuple(d -> d + length(oindA), length(oindB))...)
-    sizeC = (map(d -> size(A,d), oindA)..., map(d -> size(B,d), oindB)...)
+    cindB = NamedDims.dim(LB, s)
+    oindB = Tuple(setdiff(1:NB, cindB))
 
+    indCinoAB = Tuple(vcat(1:length(oindA), length(oindA) .+ (1:length(oindB))))
     TC = Base.promote_type(TA,TB)
-    C = similar(nameless(A), TC, sizeC)
-    contract!(true, nameless(A), :N, nameless(B), :N, false, C, oindA, cindA, oindB, cindB, indCinoAB)
+    # @show oindA cindA oindB cindB indCinoAB
 
-    ndims(C) == 0 && return first(C)
+    sizeC = (map(d -> :(size(A,$d)), oindA)..., map(d -> :(size(B,$d)), oindB)...)
+    sizeCint = :(map(Int, ($(sizeC...),) )) # avoid NamedInt!
+    out = quote
+        C = similar(nameless(A), $TC, $sizeCint)
+        contract!(true, nameless(A), :N, nameless(B), :N, false, C, $oindA, $cindA, $oindB, $cindB, $indCinoAB)
+    end
 
     LC = (map(d -> LA[d], oindA)..., map(d -> LB[d], oindB)...)
-    NamedDimsArray(C, LC)
+    if length(sizeC) == 0
+        push!(out.args, :(first(C)))
+    else
+        push!(out.args, :(NamedDimsArray{$LC}(C)))
+    end
+    out
 end
 
 
 # Hack to let you conjugate one array easily?
-for N in 3:10
-    @eval Base.adjoint(A::NamedDimsArray{L,T,$N}) where {L,T} = Adjoint(A)
+# for N in 3:10
+#     @eval Base.adjoint(A::NamedDimsArray{L,T,$N}) where {L,T} = Adjoint(A)
+# end
+
+#=
+using NamedPlus, TensorOperations
+
+@named begin
+    m{i,j} = rand(Int8, 3,4)
+    g = [n^i for n in 1:20, i in 1:3]
 end
 
+@btime mul($m, $g) # 485.713 ns
+@btime mul($m, $g, :i) # 490
+
+@btime contract($m, $g) # 1.213 μs, was 4.270 μs before adding @generated stuff
+
+@btime contract($m, $g, :i)
+@btime (() -> contract($m, $g, :i))() # 5.244 μs
+@btime contract($m, $g, Val((:i,)))   # 1.211 μs
+
+cc(m,g) = contract!(true, parent(m), :N, parent(g), :N, false, similar(parent(g),4,20),
+    (2,), (1,), (1,), (2,), (1,2))
+ccn(m,g) = NamedDimsArray(
+    contract!(true, parent(m), :N, parent(g), :N, false, similar(parent(g),4,20),
+    (2,), (1,), (1,), (2,), (1,2)), (:j, :n))
+
+@btime cc($m, $g) # 1.207 μs
+@btime ccn($m, $g) # 1.964 μs
+
+@code_warntype mul(m, g) # ok!
+@code_warntype contract(m, g) # now ok!
+@code_warntype cc(m, g)
+@code_warntype ccn(m, g)
+
+=#
+# contract!(true, nameless(A), :N, nameless(B), :N, false, C, $oindA, $cindA, $oindB, $cindB, $indCinoAB)
 
 #################### GRADIENT ####################
 # Same as in TensorTrack.jl
